c505f3c7f53eecd02ba27b65ceddb1b3a3b26609
asimov <seyranmustafa4@gmail.com>
2024-11-14 10:11:26 +0300

    p

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,3 @@
-// import About from "./pages/About";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-
-// import NewBlog from "./pages/NewBlog";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import About from "../pages/About";
